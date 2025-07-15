db7728d6adfdd0338ff72a3ece3f879168fd10bf
Komal Ayub <komalayubkomalayub79@gmail.com>
2025-07-15 20:10:53 +0500

    commit

--- my-ecommerce-website/src/sanity/env.ts
+++ my-ecommerce-website/src/sanity/env.ts
@@ -10,11 +10,6 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
-export const token = assertValue(
-  process.env.SANITY_TOKEN,
-  'Missing environment variable: SANITY_TOKEN'
-)
-
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
